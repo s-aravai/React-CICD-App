0fd84ab2970b04e72825ed2c98397dc918a9ad95
Salman Aravai <salmanaravai786@gmail.com>
2026-03-26 13:05:01 -0600

    new update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react cicd app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/This is a React CICD App/i);
   expect(linkElement).toBeInTheDocument();
 });
